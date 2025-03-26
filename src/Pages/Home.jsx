21a330e79467c1f54d02da8340928b9bc40a093a
adyaananyamishra <adyaananyamishra@email.com>
2025-03-26 14:00:05 +0530

    first

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Banner from "../components/Banner";
 import PropertyList from "../components/PropertyList";
-import Newsletter from "../components/Newsletter";
+// import Newsletter from "../components/Newsletter";
 import Reviews from "../components/Reviews";
 import TrustedPartners from "../components/TrustedPartners";
 const Home = () => {
@@ -11,7 +11,7 @@ const Home = () => {
       <PropertyList />
        <Reviews/>
        <TrustedPartners/> 
-      <Newsletter />   
+      {/* <Newsletter />    */}
     </div>
   );
 };
