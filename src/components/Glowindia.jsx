40b539c9e8c304955dbf96af18742ad18cfc313c
adyaananyamishra <adyaananyamishra@email.com>
2025-03-29 13:25:42 +0530

    chore:updated glowindia

--- src/components/Glowindia.jsx
+++ src/components/Glowindia.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Glowindia = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200 p-4 mt-4">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-300 p-4 mt-4">
       <h1 className="text-4xl font-bold text-rose-800 mb-6">Glow India Parivar</h1>
       <img 
         src="/assets/trip-14.jpg" 
